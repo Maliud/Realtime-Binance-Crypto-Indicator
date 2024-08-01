f4d5112d2299d0678dafd7f546507f8853cb9d3b
Muhammed Ali Ud <m.aliud@outlook.com>
2024-08-01 12:12:58 +0300

    vson

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
+	"sort"
 	"strconv"
 	"time"
 
-	"github.com/VictorLowther/btree"
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
 	"github.com/gorilla/websocket"
-	"github.com/mattn/go-runewidth"
-	"github.com/nsf/termbox-go"
 )
 
 const wsendpoint = "wss://fstream.binance.com/stream?streams=btcusdt@markPrice/btcusdt@depth"
@@ -19,171 +19,117 @@ var (
 	HEIGHT        = 0
 	currMarkPrice = 0.0
 	prevMarkPrice = 0.0
+	fundingRate   = "n/a"
 	ARROW_UP      = "↑"
 	ARROW_DOWN    = "↓"
 )
-
-func byBestBid(a, b *OrderbookEntry) bool {
-	return a.Price >= b.Price
-}
-
-func byBestAsk(a, b *OrderbookEntry) bool {
-	return a.Price < b.Price
-}
 
 type OrderbookEntry struct {
 	Price  float64
 	Volume float64
 }
 
+type byBestAsk []OrderbookEntry
+
+func (a byBestAsk) Len() int           { return len(a) }
+func (a byBestAsk) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byBestAsk) Less(i, j int) bool { return a[i].Price < a[j].Price }
+
+type byBestBid []OrderbookEntry
+
+func (a byBestBid) Len() int           { return len(a) }
+func (a byBestBid) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byBestBid) Less(i, j int) bool { return a[i].Price > a[j].Price }
+
 type Orderbook struct {
-	Asks *btree.Tree[*OrderbookEntry]
-	Bids *btree.Tree[*OrderbookEntry]
+	Asks map[float64]float64
+	Bids map[float64]float64
 }
 
 func NewOrderbook() *Orderbook {
 	return &Orderbook{
-		Asks: btree.New(byBestAsk),
-		Bids: btree.New(byBestBid),
-	}
-}
-
-func getBidByPrice(price float64) btree.CompareAgainst[*OrderbookEntry] {
-	return func(e *OrderbookEntry) int {
-		switch {
-		case e.Price > price:
-			return -1
-		case e.Price < price:
-			return 1
-		default:
-			return 0
-		}
-	}
-}
-
-func getAskByPrice(price float64) btree.CompareAgainst[*OrderbookEntry] {
-	return func(e *OrderbookEntry) int {
-		switch {
-		case e.Price < price:
-			return -1
-		case e.Price > price:
-			return 1
-		default:
-			return 0
-		}
+		Asks: make(map[float64]float64),
+		Bids: make(map[float64]float64),
 	}
 }
 
 func (ob *Orderbook) handleDepthResponse(asks, bids []any) {
-	//fmt.Println(reflect.TypeOf(asks))
 	for _, v := range asks {
 		ask := v.([]any)
 		price, _ := strconv.ParseFloat(ask[0].(string), 64)
 		volume, _ := strconv.ParseFloat(ask[1].(string), 64)
-
-		if entry, ok := ob.Asks.Get(getAskByPrice(price)); ok {
-			if volume == 0 {
-				ob.Asks.Delete(entry)
-			} else {
-				entry.Volume = volume
-			}
-			continue
-		}
-
-		entry := &OrderbookEntry{
-			Price:  price,
-			Volume: volume,
-		}
-		ob.Asks.Insert(entry)
-
+		ob.addAsk(price, volume)
 	}
-
 	for _, v := range bids {
-		bid := v.([]any)
-		price, _ := strconv.ParseFloat(bid[0].(string), 64)
-		volume, _ := strconv.ParseFloat(bid[1].(string), 64)
+		ask := v.([]any)
+		price, _ := strconv.ParseFloat(ask[0].(string), 64)
+		volume, _ := strconv.ParseFloat(ask[1].(string), 64)
+		ob.addBid(price, volume)
+	}
+}
 
-		if entry, ok := ob.Bids.Get(getBidByPrice(price)); ok {
-			if volume == 0 {
-				ob.Bids.Delete(entry)
-			} else {
-				entry.Volume = volume
-			}
+func (ob *Orderbook) addBid(price, volume float64) {
+	if _, ok := ob.Bids[price]; ok {
+		if volume == 0.0 {
+			delete(ob.Bids, price)
+			return
+		}
+	}
+	ob.Bids[price] = volume
+}
+
+func (ob *Orderbook) addAsk(price, volume float64) {
+	if volume == 0 {
+		delete(ob.Asks, price)
+		return
+	}
+	ob.Asks[price] = volume
+}
+
+func (ob *Orderbook) getBids() []OrderbookEntry {
+	depth := 10
+	entries := make(byBestBid, len(ob.Bids))
+	i := 0
+	for price, volume := range ob.Bids {
+		if volume == 0 {
 			continue
 		}
-
-		entry := &OrderbookEntry{
+		entries[i] = OrderbookEntry{
 			Price:  price,
 			Volume: volume,
 		}
-		ob.Bids.Insert(entry)
-
-	}
-}
-
-func (ob *Orderbook) getBids() []*OrderbookEntry {
-	var (
-		depth = 10
-		bids  = make([]*OrderbookEntry, depth)
-		it    = ob.Bids.Iterator(nil, nil)
-		i     = 0
-	)
-
-	for it.Next() {
-		if i == depth {
-			break
-		}
-		bids[i] = it.Item()
 		i++
 	}
-	it.Release()
-	return bids
+	sort.Sort(entries)
+	// var want byBestAsk
+	// if len(entries) >= depth {
+	// 	want = byBestAsk(entries[:depth])
+	// } else {
+	// 	want = byBestAsk(entries)
+	// }
+	// sort.Sort(want)
+	if len(entries) >= depth {
+		return entries[:depth]
+	}
+	return entries
 }
 
-func (ob *Orderbook) getAsks() []*OrderbookEntry {
-	var (
-		depth = 10
-		asks  = make([]*OrderbookEntry, depth)
-		it    = ob.Asks.Iterator(nil, nil)
-		i     = 0
-	)
-
-	for it.Next() {
-		if i == depth {
-			break
+func (ob *Orderbook) getAsks() []OrderbookEntry {
+	depth := 10
+	entries := make(byBestAsk, len(ob.Asks))
+	i := 0
+	for price, volume := range ob.Asks {
+		entries[i] = OrderbookEntry{
+			Price:  price,
+			Volume: volume,
 		}
-		asks[i] = it.Item()
 		i++
 	}
-	it.Release()
-	return asks
-}
-
-func (ob *Orderbook) render(x, y int) {
-	// sipariş defteri sağ çerçeve kenarlığını oluştur
-	for i := 0; i < HEIGHT; i++ {
-		termbox.SetCell(WIDTH-22, i, '|', termbox.ColorWhite, termbox.ColorDefault)
+	sort.Sort(entries)
+	if len(entries) >= depth {
+		return entries[:depth]
 	}
-
-	for i, ask := range ob.getAsks() {
-		if ask == nil {
-			continue
-		}
-		price := fmt.Sprintf("%.2f", ask.Price)
-		volume := fmt.Sprintf("%.2f", ask.Volume)
-		renderText(x, y+i, price, termbox.ColorRed)
-		renderText(x+10, y+i, volume, termbox.ColorCyan)
-	}
-
-	for i, bid := range ob.getBids() {
-		if bid == nil {
-			continue
-		}
-		price := fmt.Sprintf("%.2f", bid.Price)
-		volume := fmt.Sprintf("%.2f", bid.Volume)
-		renderText(x, 10+i, price, termbox.ColorGreen)
-		renderText(x+10, 10+i, volume, termbox.ColorCyan)
-	}
+	return entries
 }
 
 type BinanceTradeResult struct {
@@ -193,7 +139,7 @@ type BinanceTradeResult struct {
 }
 
 type BinanceDepthResult struct {
-	// fiyat teklif | büyüklüğü (volume)
+	// price | size (volume)
 	Asks [][]string `json:"a"`
 	Bids [][]string `json:"b"`
 }
@@ -204,20 +150,18 @@ type BinanceDepthResponse struct {
 }
 
 func main() {
-	termbox.Init()
-	WIDTH, HEIGHT = termbox.Size()
+	if err := ui.Init(); err != nil {
+		log.Fatal(err)
+	}
 
 	conn, _, err := websocket.DefaultDialer.Dial(wsendpoint, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	var (
-		ob = NewOrderbook()
-		//result BinanceDepthResponse
+		ob     = NewOrderbook()
 		result map[string]any
 	)
-
 	go func() {
 		for {
 			if err := conn.ReadJSON(&result); err != nil {
@@ -230,85 +174,76 @@ func main() {
 				bids := data["b"].([]any)
 				ob.handleDepthResponse(asks, bids)
 			}
-			// 3 sn önce
+			// each 3s
 			if stream == "btcusdt@markPrice" {
 				prevMarkPrice = currMarkPrice
 				data := result["data"].(map[string]any)
 				priceStr := data["p"].(string)
+				fundingRate = data["r"].(string)
 				currMarkPrice, _ = strconv.ParseFloat(priceStr, 64)
-
 			}
-
 		}
 	}()
 
 	isrunning := true
-	eventch := make(chan termbox.Event, 1)
 
-	go func() {
-		for {
-			eventch <- termbox.PollEvent()
-		}
-	}()
+	margin := 2
+	pheight := 3
 
+	pticker := widgets.NewParagraph()
+	pticker.Title = "Binance"
+	pticker.Text = "[BTCUSDT](fg:cyan)"
+	pticker.SetRect(0, 0, 14, pheight)
+
+	pprice := widgets.NewParagraph()
+	pprice.Title = "Piyasa Fiyatı"
+	ppriceOffset := 14 + 14 + margin + 2
+	pprice.SetRect(14+margin, 0, ppriceOffset, pheight)
+
+	pfund := widgets.NewParagraph()
+	pfund.Title = "Finansman Oranı"
+	pfund.SetRect(ppriceOffset+margin, 0, ppriceOffset+margin+16, 3)
+
+	tob := widgets.NewTable()
+	out := make([][]string, 20)
+	for i := 0; i < 20; i++ {
+		out[i] = []string{"n/a", "n/a"}
+	}
+	tob.TextStyle = ui.NewStyle(ui.ColorWhite)
+	tob.SetRect(0, pheight+2, 30, 22+pheight+2)
+	tob.PaddingBottom = 0
+	tob.PaddingTop = 0
+	tob.RowSeparator = false
+	tob.TextAlignment = ui.AlignCenter
 	for isrunning {
-		select {
-		case event := <-eventch:
-			switch event.Key {
-			case termbox.KeyEsc:
-				isrunning = false
+		var (
+			asks = ob.getAsks()
+			bids = ob.getBids()
+			// it   = 0
+		)
+		if len(asks) >= 10 {
+			for i := 0; i < 10; i++ {
+				out[i] = []string{fmt.Sprintf("[%.2f](fg:red)", asks[i].Price), fmt.Sprintf("[%.2f](fg:cyan)", asks[i].Volume)}
 			}
-		default:
 		}
+		if len(bids) >= 10 {
+			for i := 0; i < 10; i++ {
+				out[i+10] = []string{fmt.Sprintf("[%.2f](fg:green)", bids[i].Price), fmt.Sprintf("[%.2f](fg:cyan)", bids[i].Volume)}
+			}
+		}
+		tob.Rows = out
 
-		termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-		render()
-		ob.render(WIDTH-18, 2)
-		time.Sleep(time.Millisecond * 16)
-		termbox.Flush()
+		pprice.Text = getMarketPrice()
+		pfund.Text = fmt.Sprintf("[%s](fg:yellow)", fundingRate)
+		ui.Render(pticker, pprice, pfund, tob)
+		time.Sleep(time.Millisecond * 20)
 	}
 }
 
-func renderMarketPrice() {
-	color := termbox.ColorRed
-	arrow := ARROW_DOWN
-	if currMarkPrice > prevMarkPrice {
-		color = termbox.ColorGreen
-		arrow = ARROW_UP
+func getMarketPrice() string {
+	price := fmt.Sprintf("[%s %.2f](fg:green)", ARROW_UP, currMarkPrice)
+	if prevMarkPrice > currMarkPrice {
+		price = fmt.Sprintf("[%s %.2f](fg:red)", ARROW_DOWN, currMarkPrice)
 	}
-	renderText(2, 1, fmt.Sprintf("%.2f", currMarkPrice), color)
-	renderText(11, 1, string(arrow), color)
-}
-
-func render() {
-	// pencere çerçevesi kenarlığını oluştur
-	for i := 0; i < WIDTH; i++ {
-		termbox.SetCell(i, 0, '-', termbox.ColorWhite, termbox.ColorDefault)
-		termbox.SetCell(i, HEIGHT-1, '-', termbox.ColorWhite, termbox.ColorDefault)
-	}
-
-	for i := 0; i < HEIGHT; i++ {
-		termbox.SetCell(0, i, '|', termbox.ColorWhite, termbox.ColorDefault)
-		termbox.SetCell(WIDTH-1, i, '|', termbox.ColorWhite, termbox.ColorDefault)
-	}
-
-	for i := 0; i < WIDTH; i++ {
-		termbox.SetCell(i, 2, '-', termbox.ColorWhite, termbox.ColorDefault)
-
-	}
-
-	renderMarketPrice()
-}
-
-func renderLine(start, end int, color termbox.Attribute) {
-
-}
-
-func renderText(x int, y int, msg string, color termbox.Attribute) {
-	for _, ch := range msg {
-		termbox.SetCell(x, y, ch, color, termbox.ColorDefault)
-
-		w := runewidth.RuneWidth(ch)
-		x += w
-	}
+	return price
 }
